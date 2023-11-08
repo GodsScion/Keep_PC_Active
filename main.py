f90c2fa4fab329359384ea571c4f5287db8683b6
Sai Vignesh Golla <100998531+GodsScion@users.noreply.github.com>
2023-11-08 15:20:21 -0800

    Better Messages

--- main.py
+++ main.py
@@ -4,24 +4,22 @@ LinkedIn: https://www.linkedin.com/in/saivigneshgolla
 Version: 1.0.0
 '''
 
-############# Config #############
+########################### Config ###########################
 
 # Total time the program needs to run in seconds
-total_time =  8 * 60 * 60 
+total_time =    8*60*60 + 30*60 + 0
 
 # Time between each press in seconds
-intervals =  30
+intervals =     30
 
 # How many seconds would you like to extend time when asked?
-extend_time =  30 * 60
+extend_time =   1*60*60
 
-##################################
-
+##############################################################
 
 
 import pyautogui
 pyautogui.FAILSAFE = False
-
 
 def countdown(message, timeLeft):
     try:
@@ -32,10 +30,18 @@ def countdown(message, timeLeft):
             pyautogui.press('altleft')  # Or replace with 'shiftleft' if you want to use Left Shift key instead of Left Alt key.
             pyautogui.sleep(min(intervals, timeLeft))
             timeLeft -= intervals
-        if pyautogui.confirm("Extend time by {} secs?".format(extend_time), buttons=['Yes','No']) == "Yes": countdown("Extended time.\nWhen done press 'Ctrl + C' or close this window to exit.", extend_time)
+        h = int(extend_time/3600)
+        m = int(extend_time/60 - h*60)
+        s = extend_time%60
+        if pyautogui.confirm("Extend time by  {} hr {} min {} sec?".format(h, m, s), buttons=['Yes','No']) == "Yes":
+            countdown("Extended time by  {} hr {} min {} sec.\nWhen done press 'Ctrl + C' or close this window to exit.".format(h, m, s), extend_time)
     except KeyboardInterrupt:
         print("Script terminated by the user")
 
 if __name__ == "__main__":
-    try:    countdown("Program started. Will run for {} secs.\nFeel free to minimize the screen and carry on with your work.\nWhen done press 'Ctrl + C' or close this window to exit.".format(total_time), total_time)
-    except Exception as error:  print("Error Occurred! ",error)
+    try:
+        h = int(total_time/3600)
+        m = int(total_time/60 - h*60)
+        countdown("Program started. Will run for  {} hr {} min {} sec.\nFeel free to minimize the screen and carry on with your work.\nWhen done press 'Ctrl + C' or close this window to exit.".format(h, m, total_time%60), total_time)
+    except Exception as error:
+        print("Error Occurred! ",error)
