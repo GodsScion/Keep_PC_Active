8563540ff5ca64a5ad42faf1369fec3e6c3045b9
Sai Vignesh Golla <100998531+GodsScion@users.noreply.github.com>
2023-11-09 14:39:10 -0800

    moved print msg from countdown function

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ import pyautogui
 pyautogui.FAILSAFE = False
 from config import *
 
-def countdown(message, timeLeft):
+def countdown(timeLeft):
     try:
-        print(message)
         while timeLeft > 0:
             # pyautogui.move(+1, 0)
             # pyautogui.move(-1, 0)
@@ -21,7 +20,8 @@ def countdown(message, timeLeft):
         m = int(extend_time/60 - h*60)
         s = extend_time%60
         if pyautogui.confirm("Extend time by  {} hr {} min {} sec?".format(h, m, s), buttons=['Yes','No']) == "Yes":
-            countdown("Extended time by  {} hr {} min {} sec.\nWhen done press 'Ctrl + C' or close this window to exit.".format(h, m, s), extend_time)
+            print("Extended time by  {} hr {} min {} sec.\nWhen done press 'Ctrl + C' or close this window to exit.".format(h, m, s))
+            countdown(extend_time)
     except KeyboardInterrupt:
         print("Script terminated by the user")
 
@@ -29,6 +29,7 @@ if __name__ == "__main__":
     try:
         h = int(total_time/3600)
         m = int(total_time/60 - h*60)
-        countdown("Program started. Will run for  {} hr {} min {} sec.\nFeel free to minimize the screen and carry on with your work.\nWhen done press 'Ctrl + C' or close this window to exit.".format(h, m, total_time%60), total_time)
+        print("Program started. Will run for  {} hr {} min {} sec.\nFeel free to minimize the screen and carry on with your work.\nWhen done press 'Ctrl + C' or close this window to exit.".format(h, m, total_time%60))
+        countdown(total_time)
     except Exception as error:
         print("Error Occurred! ",error)
