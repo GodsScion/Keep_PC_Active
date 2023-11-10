b96bf9ff5acb2c4d37c0e6d1c5479f54e65b2131
Sai Vignesh Golla <100998531+GodsScion@users.noreply.github.com>
2023-11-10 09:38:38 -0800

    Added print time left

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 '''
 Author:     Sai Vignesh Golla
 LinkedIn:   https://www.linkedin.com/in/saivigneshgolla
-Version:    1.0.0
+Version:    1.1.0
 '''
 
 import pyautogui
 pyautogui.FAILSAFE = False
 from config import *
 
+def actions():
+    pyautogui.press('shiftright')
+    # pyautogui.move(+1, 0)
+    # pyautogui.move(-1, 0)
+
+def convertTime(secs):
+    h = int(secs/3600)
+    return (h, int(secs/60 - h*60), secs%60)
+
+def printTimeLeft(timeLeft):
+    h, m, s = convertTime(timeLeft)
+    print("Time Left: {} hr {} min".format(h,m), end="\r") if h > 0 or m > 0 else print("Less than a minute left...", end="\r")
+
 def countdown(timeLeft):
-    try:
-        while timeLeft > 0:
-            # pyautogui.move(+1, 0)
-            # pyautogui.move(-1, 0)
-            pyautogui.press('shiftright')
-            pyautogui.sleep(min(intervals, timeLeft))
-            timeLeft -= intervals
-        h = int(extend_time/3600)
-        m = int(extend_time/60 - h*60)
-        s = extend_time%60
-        if pyautogui.confirm("Extend time by  {} hr {} min {} sec?".format(h, m, s), buttons=['Yes','No']) == "Yes":
-            print("Extended time by  {} hr {} min {} sec.\nWhen done press 'Ctrl + C' or close this window to exit.".format(h, m, s))
-            countdown(extend_time)
-    except KeyboardInterrupt:
-        print("Script terminated by the user")
+    while timeLeft > 0:
+        currentInterval = intervals
+        actions()
+        while currentInterval >= 60:
+            printTimeLeft(timeLeft)
+            pyautogui.sleep(60)
+            currentInterval -= 60
+        pyautogui.sleep(min(currentInterval, timeLeft))
+        timeLeft -= currentInterval
+        printTimeLeft(timeLeft)
+    h, m, s = convertTime(extend_time)
+    if pyautogui.confirm("Extend time by  {} hr {} min {} sec?".format(h, m, s), buttons=['Yes','No']) == "Yes":
+        print("Extended time by  {} hr {} min {} sec.\nWhen done press 'Ctrl + C' or close this window to exit.\n...".format(h, m, s))
+        countdown(extend_time)
 
 if __name__ == "__main__":
     try:
-        h = int(total_time/3600)
-        m = int(total_time/60 - h*60)
-        print("Program started. Will run for  {} hr {} min {} sec.\nFeel free to minimize the screen and carry on with your work.\nWhen done press 'Ctrl + C' or close this window to exit.".format(h, m, total_time%60))
+        h, m, s = convertTime(total_time)
+        print("Program started. Will run for  {} hr {} min {} sec.\nFeel free to minimize the screen and carry on with your work.\nWhen done press 'Ctrl + C' or close this window to exit.\n...".format(h, m, s))
         countdown(total_time)
+    except KeyboardInterrupt:
+        print("Script terminated by the user")
     except Exception as error:
         print("Error Occurred! ",error)
